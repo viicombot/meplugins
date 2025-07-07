53fa456cde8b14f483f8e696cf4e757f6a7290ce
navycodes <149182882+navycodes@users.noreply.github.com>
2025-07-08 04:38:40 +0700

    a

---
+++ chatgpt.py
@@ -0,0 +1,38 @@
+import traceback
+import config
+
+from pyrogram import filters
+from core import app
+from utils.functions import Tools #update_user_data 
+#from utils.decorators import Checklimit
+from logs import LOGGER
+
+
+__MODULE__ = "ChatGpt"
+__HELP__ = """
+<blockquote expandable>
+**You can answer question to ChatGpt3** 
+    <b>★ /blackbox</b> (question) </blockquote>
+"""
+
+
+@app.on_message(filters.command(["ask"]) & ~config.BANNED_USERS)
+#@Checklimit("chatgpt")
+async def chatgpt_cmd(client, message):
+    proses = await message.reply(">**Proses your request...**")
+    if len(message.command) < 2:
+        return await proses.edit(
+            f">**Please give a prompt or question.**"
+        )
+    prompt = client.get_text(message)
+    payload = [{"role": "system", "content": f"kamu adalah asisten virtual diaplikasi telegram bernama {client.name} yang ceria"}, {"role": "user", "content": prompt.strip()}]
+    try:
+        r = await Tools.fetch.post("https://api.siputzx.my.id/api/ai/gpt3", json=payload)
+        if r.status_code != 200:
+            return await proses.edit(">**Please try again later, maybe server is down.**")
+        data = r.json()
+        #await update_user_data(client, user_id, "blackboxquery", True)
+        return await proses.edit(data.get("data"))
+    except Exception as e:
+        LOGGER.error(traceback.format_exc())
+        return await proses.edit(f">**Terjadi kesalahan:**\n`{e}`")
